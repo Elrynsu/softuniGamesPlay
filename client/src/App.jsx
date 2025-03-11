0f07badd89c2d3aa6d1914aa9c89a0f5472e312d
Teodor <elrynsier@gmail.com>
2025-03-12 00:00:37 +0200

    add react routes with home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
+import { Routes, Route} from 'react-router';
+
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+
 import './App.css'
 
 function App() {
 
-  return (
-    <div id="box">
-        <h1>Hello router</h1>
-    </div>
-  )
+    return (
+        <div id="box">
+            <Header />
+            <main id="main-content">
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </main>
+        </div>
+    )
 }
 
 export default App
