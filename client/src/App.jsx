43c57afc7eb16148be5da76fdeda8aa4365b374e
Teodor <elrynsier@gmail.com>
2025-03-12 00:20:38 +0200

    add all pages and routes to them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import { Routes, Route} from 'react-router';
 
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import GameCreate from './components/game-create/GameCreate';
+import GameCatalog from './components/game-catalog/gameCatalog';
+import GameDetails from './components/game-details/GameDetails';
+import GameEdit from './components/game-edit/GameEdit';
 
 import './App.css'
 
@@ -13,6 +19,12 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/games' element={<GameCatalog />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/games/create' element={<GameCreate />} />
+                    <Route path='/games/:id/edit' element={<GameEdit />} />
+                    <Route path='/games/:id/details' element={<GameDetails />} />
                 </Routes>
             </main>
         </div>
